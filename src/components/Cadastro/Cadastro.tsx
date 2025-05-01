20b898e914d8afcc5087f5531a47aca818343bd1
felipeanselmosc <fe.felipe2283@gmail.com>
2025-05-01 17:44:34 -0300

    colocando logica do cadastro

--- src/components/Cadastro/Cadastro.tsx
+++ src/components/Cadastro/Cadastro.tsx
@@ -17,4 +17,27 @@ export default function Cadastro() {
       const { name, value } = e.target;
       setUsuario({ ...usuario, [name]: value });
     };
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        const listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios") || "[]");
+        const novoUsuario = {
+          id: listaUsuarios.length + 1,
+          nome: usuario.nome,
+          email: usuario.email,
+          senha: usuario.senha
+        };
+    
+        listaUsuarios.push(novoUsuario);
+        localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios));
+        alert("Usuário cadastrado com sucesso!");
+    
+        setUsuario({
+          id: 0,
+          nome: "",
+          email: "",
+          senha: ""
+        });
+      };
+    
 }
